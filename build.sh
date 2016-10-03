3bb62409a3c604bc170a487eb37105679a85539b
Florian Delizy <florian.delizy@gmail.com>
2016-10-04 00:34:13 +0200

    adding customization threw build.sh script

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+echo "// DO NOT MODIFY THIS FILE, ALL YOUR MODIFICATIONS WILL BE LOST" > sass/_overrides.scss
+
+if [ -n "$1" ]; then
+	case $1 in
+		--help|-h)
+			echo "Usage $0 [path-to-overrides.scss]"
+			exit
+			;;
+		*)
+			if [ ! -f $1 ]; then
+				echo "$1 file not found"
+				exit 2
+			fi
+			echo "Applying $1 to sass/_overrides.scss"
+			cat $1 >> sass/_overrides.scss
+			;;
+		
+	esac 
+fi
+
 [ ! -d sass ] && bundle install
 [ ! -d bourbon ] && bourbon install
 [ ! -d neat ] && neat install
